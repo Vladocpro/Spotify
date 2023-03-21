6447c11aa34e97bde3f117e05ba3c82e04d3a058
vladocpro <infernohs9@gmail.com>
2023-03-21 22:49:27 +0000

    Deploy

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect} from 'react';
 import {getProviders, signIn, useSession} from "next-auth/react";
-import {useRouter} from "next/router";
 
 const Login = ({providers} : any) => {
 
